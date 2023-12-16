7176f4b6e1530ce86389ec9990012370c5302687
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-12-16 11:03:54 -0500

    added gameboard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare() {
+    setGameBoard((prevGameBoard) => {
+      const updateBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      prevGameBoard[rowIndex][colIndex] = "X";
+      return prevGameBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
